44a13637e55e0bf977afdbb785d67eac7e476570
Ritish Bhatoye <ritishbhatoye@gmail.com>
2026-09-05 17:08:12 +0530

    update: 01_core_python/level_1_beginner/solutions/003_dictionary_of_squares.py

--- 01_core_python/level_1_beginner/solutions/003_dictionary_of_squares.py
+++ 01_core_python/level_1_beginner/solutions/003_dictionary_of_squares.py
@@ -3,11 +3,13 @@
 Generate a dictionary that contains (i, i*i) for i from 1 to n (both included).
 """
 
-
 # Your implementation here
-pass
+squares = {}
+
+n = int(input("Enter the number :- "))
+
+for index, num in enumerate(range(1, n + 1)):
+    squares[index] = index * index
 
 
-if __name__ == "__main__":
-    # Test your solution
-    print("Implement me!")
+print(squares)
