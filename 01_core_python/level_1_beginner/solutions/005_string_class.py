2f92b012555a012b1b4a27691fd5bea2594ef890
Ritish Bhatoye <ritishbhatoye@gmail.com>
2026-09-06 20:48:12 +0530

    update: 01_core_python/level_1_beginner/solutions/005_string_class.py

--- 01_core_python/level_1_beginner/solutions/005_string_class.py
+++ 01_core_python/level_1_beginner/solutions/005_string_class.py
@@ -3,11 +3,19 @@
 Define a class with getString and printString methods. printString prints in uppercase.
 """
 
-
 # Your implementation here
 pass
 
 
-if __name__ == "__main__":
-    # Test your solution
-    print("Implement me!")
+class String:
+    def getString(self):
+        self.s = input("Enter the string")
+
+    def printString(self):
+        print(self.s.upper())
+
+
+s = String()
+
+s.getString()
+s.printString()
