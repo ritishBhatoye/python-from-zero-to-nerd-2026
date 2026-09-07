446f50302b165ab0769a72ec4fd9f5b821507f09
Ritish Bhatoye <ritishbhatoye@gmail.com>
2026-09-07 20:47:17 +0530

    update: 01_core_python/level_1_beginner/solutions/032_even_or_odd_checker.py

--- 01_core_python/level_1_beginner/solutions/032_even_or_odd_checker.py
+++ 01_core_python/level_1_beginner/solutions/032_even_or_odd_checker.py
@@ -3,11 +3,16 @@
 Print 'It is an even number' or 'It is an odd number' based on input.
 """
 
-
 # Your implementation here
 pass
 
 
-if __name__ == "__main__":
-    # Test your solution
-    print("Implement me!")
+def isEvenOrOdd(n):
+    if n % 2 == 0:
+        print("It is an even number")
+    else:
+        print("It is an odd number")
+
+
+n = int(input("Enter the number :- "))
+isEvenOrOdd(n)
