f9d5b2742494d56e7c4b539bb0561c33add16faa
Ritish Bhatoye <ritishbhatoye@gmail.com>
2026-09-07 17:59:51 +0530

    update: 01_core_python/level_1_beginner/solutions/027_integer_to_string_converter.py

--- 01_core_python/level_1_beginner/solutions/027_integer_to_string_converter.py
+++ 01_core_python/level_1_beginner/solutions/027_integer_to_string_converter.py
@@ -5,3 +5,14 @@ Define a function that converts an integer into a string and prints it.
 
 # Your implementation here
 pass
+
+
+def toString(a):
+    return str(a)
+
+
+a = int(input("Enter the number : "))
+
+print("Type of ", a, type(a))
+toString(a)
+print("Type of ", a, " after conversion ", type(a))
