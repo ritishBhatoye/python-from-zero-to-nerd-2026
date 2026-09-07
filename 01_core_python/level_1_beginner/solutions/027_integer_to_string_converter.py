1d43f2cc672dc564a4f0fd69bee5f84c37f476e5
Ritish Bhatoye <ritishbhatoye@gmail.com>
2026-09-07 17:51:49 +0530

    update: 01_core_python/level_1_beginner/solutions/027_integer_to_string_converter.py

--- 01_core_python/level_1_beginner/solutions/027_integer_to_string_converter.py
+++ 01_core_python/level_1_beginner/solutions/027_integer_to_string_converter.py
@@ -3,11 +3,5 @@
 Define a function that converts an integer into a string and prints it.
 """
 
-
 # Your implementation here
 pass
-
-
-if __name__ == "__main__":
-    # Test your solution
-    print("Implement me!")
