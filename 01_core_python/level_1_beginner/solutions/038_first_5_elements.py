4cd636b33817f77ad1c973cc0f07981992730319
Ritish Bhatoye <ritishbhatoye@gmail.com>
2026-09-08 18:23:19 +0530

    update: 01_core_python/level_1_beginner/solutions/038_first_5_elements.py

--- 01_core_python/level_1_beginner/solutions/038_first_5_elements.py
+++ 01_core_python/level_1_beginner/solutions/038_first_5_elements.py
@@ -3,11 +3,14 @@
 Generate list of squares (1-20) and print first 5 elements.
 """
 
-
 # Your implementation here
 pass
 
 
-if __name__ == "__main__":
-    # Test your solution
-    print("Implement me!")
+ans = {}
+
+for n in range(1, 21):
+    ans[n] = n * n
+
+
+print(ans)
